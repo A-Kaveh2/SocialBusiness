afd3fb7e629879b00f20e22508f76d45d273ce0d
Khalili <sina_od@yahoo.com>
2015-02-06 14:24:48 +0330

    some fixes

--- app/src/main/java/ir/rasen/myapplication/FragmentProfile.java
+++ app/src/main/java/ir/rasen/myapplication/FragmentProfile.java
@@ -150,13 +150,13 @@ public class FragmentProfile extends Fragment implements WebserviceResponse, Edi
 
             //TODO remove test part
             //get user home info by sending user_id
-            new GetUserHomeInfo(LoginInfo.getUserId(cont),LoginInfo.getUserId(cont),webserviceResponse).execute();
+            new GetUserHomeInfo(profile_user.id,LoginInfo.getUserId(cont),webserviceResponse).execute();
             runningWebserviceType = RunningWebserviceType.getUserHomeInfo;
             // TODO ::
             if(profileId!=0) {
 
                 //TODO for the test
-                new GetUserHomeInfo(LoginInfo.getUserId(cont),LoginInfo.getUserId(cont), webserviceResponse).execute();
+                new GetUserHomeInfo(profile_business.id,LoginInfo.getUserId(cont), webserviceResponse).execute();
                 runningWebserviceType = RunningWebserviceType.getUserHomeInfo;
             } else {
                 // TODO :: GET with username
